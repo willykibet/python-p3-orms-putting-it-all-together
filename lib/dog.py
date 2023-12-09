6e9d07d83d7230f0cc409142667b2c21655edabf
willykibet <willykipchumba22@gmail.com>
2023-12-09 23:45:27 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -1,8 +1,25 @@
-import sqlite3
+from models import Dog
 
-CONN = sqlite3.connect('lib/dogs.db')
-CURSOR = CONN.cursor()
+def create_table(base, engine):
+    base.metadata.create_all(engine)
 
-class Dog:
-    
-    pass
+def save(session, dog):
+    session.add(dog)
+    session.commit()
+
+def get_all(session):
+    return session.query(Dog).all()
+
+def find_by_name(session, name):
+    return session.query(Dog).filter(Dog.name == name).first()
+
+def find_by_id(session, id):
+    return session.query(Dog).filter(Dog.id == id).first()
+
+def find_by_name_and_breed(session, name, breed):
+    return session.query(Dog).filter(Dog.name == name and Dog.breed == breed).first()
+
+def update_breed(session, dog, breed):
+    dog.breed = breed
+    session.add(dog)
+    session.commit()
